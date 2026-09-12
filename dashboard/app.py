e3ac0bfe6a66c93dc3dcfe0db9077572a2d6f9b0
Gaurav Singh Bhakuni <gauravbhakuni666@gmail.com>
2026-09-12 17:50:23 +0530

    Checked the working of the dashboard

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,17 @@
+import streamlit as st
+from pathlib import Path
+import sys
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT_DIR))
+
 from src.data_preprocessing import load_and_clean_data
 from src.sequence_builder import build_sequences
 
 df = load_and_clean_data("data/dataset.csv")
 
 sequences = build_sequences(df)
+
+st.title("Clinical Trial Mining")
+st.write(df.head(5))
+st.write("Patients:", len(sequences))
+st.write(sequences[:3])
